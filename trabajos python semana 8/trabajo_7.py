e0c98a787786f0835a55d6722a6bf6f6edccd7e3
villarkako <villagran.arias.87@gmail.com>
2026-04-20 22:29:49 -0400

    Update trabajo_7.py

--- trabajos python semana 8/trabajo_7.py
+++ trabajos python semana 8/trabajo_7.py
@@ -1,3 +1,8 @@
+
+print(" ******** BIENVENIDO********")
+
+
+
 def calcular_imc(altura, peso):
     return (peso/(altura * altura))
 
@@ -6,5 +11,22 @@ def main():
     peso=float(input("ingrese su peso en kilos:"))
     print(f"su indice de IMC es: {calcular_imc(altura, peso):.2f}")
 
+    resultado = calcular_imc(altura, peso)
+
+    if resultado <= 18.5:
+        print ("esta bajo peso")
+    elif resultado <= 24.9:
+        print ("Peso normal")
+    elif resultado <= 29.9:
+        print ("sobre peso")
+    else:
+        print ("obesidad")
 if __name__ == "__main__":
-    main()
+    salir= False
+    while not salir:
+        main()
+        if input("continuar (s/n):")=="n": salir=True
+
+
+
+
